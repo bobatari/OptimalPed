bfab8fd2b6e44c5f9cd122db548054bfab169b26
bobatari <aaronaordaz@gmail.com>
2024-09-26 10:06:54 -0700

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,27 @@
+Config = {}
+
+Config.Peds = {
+    {
+        model = 'jackskellington',
+        location = vector3(241.94, -1398.34, 30.53),
+        heading = 135.0
+    },
+    {
+        model = 'Brook',
+        location = vector3(245.00, -1395.00, 30.53),
+        heading = 90.0
+    },
+    {
+        model = 'Cuphead',
+        location = vector3(250.00, -1390.00, 30.53),
+        heading = 180.0
+    }
+    --[[
+    {
+        model = 'Beavis',
+        location = vector3(245.00, -1395.00, 30.53),
+        heading = 90.0
+    },
+    ]]
+    -- Add more peds here as needed
+}
