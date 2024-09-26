b5199826db2124ff9b3220000dac189ebb71d86a
bobatari <aaronaordaz@gmail.com>
2024-09-26 10:10:23 -0700

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'boba'
+description 'ped changer'
+
+shared_scripts {
+    'config.lua'
+}
+
+client_script 'client.lua'
+
+server_script 'server.lua'
+
+lua54 'yes'
